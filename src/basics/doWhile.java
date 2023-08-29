ee26455424fda705cabb88e2c82ac48d5f387d0f
Kethan Salimatti <kethansalimatti11@gmail.com>
2023-08-29 11:52:44 +0530

    Modified doWhile

--- src/basics/doWhile.java
+++ src/basics/doWhile.java
@@ -2,10 +2,10 @@ package basics;
 
 public class doWhile {
     public static void main(String args[]){
-        int i=0;
+        int i=1;
         do{
             System.out.println(i);
-            i++;
+            i = i+2;
         } while(i<=10);
     }
     
